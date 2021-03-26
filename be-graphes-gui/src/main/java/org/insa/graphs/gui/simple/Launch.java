7397b8bdd9913e6d1deadf3789018fda710f1c93
Emmanuel Pastor <paros7191@gmail.com>
2021-03-26 22:38:34 +0100

    Implement Launch class

--- be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
+++ be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
@@ -1,21 +1,19 @@
 package org.insa.graphs.gui.simple;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-
 import org.insa.graphs.gui.drawing.Drawing;
 import org.insa.graphs.gui.drawing.components.BasicDrawing;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Path;
 import org.insa.graphs.model.io.BinaryGraphReader;
+import org.insa.graphs.model.io.BinaryPathReader;
 import org.insa.graphs.model.io.GraphReader;
 import org.insa.graphs.model.io.PathReader;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 
 /**
  * @author Emmanuel Pastor
@@ -25,24 +23,20 @@ public class Launch {
 
     /**
      * Create a new Drawing inside a JFrame an return it.
-     * 
+     *
      * @return The created drawing.
-     * 
      * @throws Exception if something wrong happens when creating the graph.
      */
     public static Drawing createDrawing() throws Exception {
         BasicDrawing basicDrawing = new BasicDrawing();
-        SwingUtilities.invokeAndWait(new Runnable() {
-            @Override
-            public void run() {
-                JFrame frame = new JFrame("BE Graphes - Launch");
-                frame.setLayout(new BorderLayout());
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setVisible(true);
-                frame.setSize(new Dimension(800, 600));
-                frame.setContentPane(basicDrawing);
-                frame.validate();
-            }
+        SwingUtilities.invokeAndWait(() -> {
+            JFrame frame = new JFrame("BE Graphes - Launch");
+            frame.setLayout(new BorderLayout());
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+            frame.setSize(new Dimension(800, 600));
+            frame.setContentPane(basicDrawing);
+            frame.validate();
         });
         return basicDrawing;
     }
@@ -50,28 +44,28 @@ public class Launch {
     public static void main(String[] args) throws Exception {
 
         // Visit these directory to see the list of available files on Commetud.
-        final String mapName = "C:/Users/OMman/IdeaProjects/be-graphes/be-graphes-gui/src/main/resources/maps/insa.mapgr";
-        final String pathName = "/home/commetud/3eme Annee MIC/Graphes-et-Algorithmes/Paths/path_fr31insa_rangueil_r2.path";
+        final String mapName = "C:/Users/OMman/IdeaProjects/be-graphes/be-graphes-gui/src/main/resources/maps/haute-garonne.mapgr";
+        final String pathName = "C:/Users/OMman/IdeaProjects/be-graphes/be-graphes-gui/src/main/resources/paths/path_fr31_insa_bikini_canal.path";
 
         // Create a graph reader.
         final GraphReader reader = new BinaryGraphReader(
-                new DataInputStream(new BufferedInputStream(new FileInputStream(mapName))));
+                new DataInputStream(new BufferedInputStream(new FileInputStream(mapName)))
+        );
 
-        // TODO: Read the graph.
-        final Graph graph = null;
+        final Graph graph = reader.read();
 
         // Create the drawing:
         final Drawing drawing = createDrawing();
 
-        // TODO: Draw the graph on the drawing.
+        drawing.drawGraph(graph);
 
-        // TODO: Create a PathReader.
-        final PathReader pathReader = null;
+        final PathReader pathReader = new BinaryPathReader(
+                new DataInputStream(new BufferedInputStream(new FileInputStream(pathName)))
+        );
 
-        // TODO: Read the path.
-        final Path path = null;
+        final Path path = pathReader.readPath(graph);
 
-        // TODO: Draw the path.
+        drawing.drawPath(path, Color.GREEN);
     }
 
 }
